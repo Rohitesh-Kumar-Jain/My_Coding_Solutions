dc2a12e1ab9c96e4b276ee17ea56f0f0e5787b5c
Rohitesh Kumar Jain <thekumarjain@gmail.com>
2021-01-16 10:35:12 +0530

    Update 1625. Lexicographically Smallest String After Applying Operations.java

--- 1625. Lexicographically Smallest String After Applying Operations.java
+++ 1625. Lexicographically Smallest String After Applying Operations.java
@@ -1,5 +1,6 @@
    // This solution is't mine solution, I have copied it from [rock](https://leetcode.com/problems/lexicographically-smallest-string-after-applying-operations/discuss/899511/JavaPython-3-BFS-and-DFS-w-brief-explanation-and-analysis.)
    
+   // DFS 
    private String smallest;
     public String findLexSmallestString(String s, int a, int b) {
         smallest = s;
@@ -18,4 +19,29 @@
             dfs(String.valueOf(ca), a, b, seen);
             dfs(s.substring(b) + s.substring(0, b), a, b, seen);
         }
+    }
+
+   
+   //BFS 
+    public String findLexSmallestString(String s, int a, int b) {
+        int n = s.length();
+        String smallest = s;
+        Queue<String> q = new LinkedList<>();
+        q.offer(s);
+        Set<String> seen = new HashSet<>(q);
+        while (!q.isEmpty()) {
+            String cur = q.poll();
+            if (smallest.compareTo(cur) > 0)
+                smallest = cur;
+            char[] ca = cur.toCharArray();
+            for (int i = 1; i < ca.length; i += 2) // add operation.
+                ca[i] = (char)((ca[i] - '0' + a) % 10 + '0');
+            String addA = String.valueOf(ca);
+            if (seen.add(addA))
+                q.offer(addA);
+            String rotate = cur.substring(n - b) + cur.substring(0, n - b); // rotation.
+            if (seen.add(rotate))
+                q.offer(rotate);
+        }
+        return smallest;
     }
