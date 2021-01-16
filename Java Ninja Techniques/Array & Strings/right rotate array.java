2dbc589112d153081be91242606c4e7b8a20b24a
Rohitesh Kumar Jain <thekumarjain@gmail.com>
2021-01-16 10:43:26 +0530

    Update right rotate array.java

--- Java Ninja Techniques/Array & Strings/right rotate array.java
+++ Java Ninja Techniques/Array & Strings/right rotate array.java
@@ -2,3 +2,13 @@
 
 String rightRotatedStringBy_b;
 rightRotatedStringBy_b = s.substring(b) + s.substring(0, b);
+
+
+
+// Another way could have been :
+
+
+String s1 = reverse(s.substring(0, b));
+String s2 = reverse(s.substring(b));
+StringBuilder sb = new StringBuilder(s1+s2);
+return sb.reverse().toString();
